e4d92b99dd46dde67bc1c02b5493c171c545aebf
Tăng Huỳnh Tuấn Tú <tanghuynhtuantuwork@gmail.com>
2026-09-19 23:16:12 +0700

    Show 'About' page link in navbar

--- frontend-gomsu/components/layout/Navbar.js
+++ frontend-gomsu/components/layout/Navbar.js
@@ -12,7 +12,7 @@ const navLinks = [
   { name: 'Bộ sưu tập', path: '/gallery' },
   // { name: 'Dự án', path: '/projects' },
   { name: 'Tin tức', path: '/blog' },
-  // { name: 'Về chúng tôi', path: '/about' },
+  { name: 'Về chúng tôi', path: '/about' },
   { name: 'Liên hệ', path: '/contact' },
 ];
 
